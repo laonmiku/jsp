ee447234b3eeb10e65199d9abf7e1f2364d15753
laonmiku <laonmiku@gmail.com>
2024-05-16 18:13:40 +0900

    5-16

---
+++ ex11/src/main/java/model/DBTest.java
@@ -0,0 +1,19 @@
+package model;
+
+public class DBTest {
+
+	public static void main(String[] args) {
+		//UserDAO dao=new UserDAO();
+		//UserVO vo=dao.read("blue");
+		//System.out.println(vo.toString());
+		
+		GoodsDAO dao=new GoodsDAO();
+		QueryVO vo = new QueryVO();
+		vo.setKey("gid");
+		vo.setWord("");
+		vo.setPage(1);
+		vo.setSize(3);
+		dao.list(vo, "red");
+	}
+
+}
